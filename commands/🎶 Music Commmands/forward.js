1ea4a506d82fb80a4c98a6eb99fe0b412bbf1a74
khoilcm123 <81813846+khoilcm123@users.noreply.github.com>
2021-06-03 13:55:38 +0700

    Update forward.js

--- commands/🎶 Music Commmands/forward.js
+++ commands/🎶 Music Commmands/forward.js
@@ -12,7 +12,7 @@ module.exports = {
                 let isdj=false;
                 let leftb = "";
                     if(client.settings.get(message.guild.id, `djroles`).join("") === "") 
-                        leftb = "❌ \`không có Kênh, hay còn gọi là tất cả các Kênh đều là Kênh Bot\`"
+                        leftb = "Không tìm thấy kênh!"
                     else
                         for(let i = 0; i < client.settings.get(message.guild.id, `djroles`).length; i++){
                                 if(message.member.roles.cache.has(client.settings.get(message.guild.id, `djroles`)[i])) isdj = true;
@@ -22,11 +22,11 @@ module.exports = {
                     if(!isdj && !message.member.hasPermission("ADMINISTRATOR")) return functions.embedbuilder(client, 6000, message, config.colors.no, "DJ-ROLE", `\`Bạn\' không có quyền cho Lệnh này! Bạn cần có: ${leftb}\``)
                 }
                 
-    if (!client.distube.isPlaying(message)) return functions.embedbuilder(client, 3000, message, config.colors.no, "❌\`Không có bài nào đang được phát\`")
-    if (!message.member.voice.channel) return functions.embedbuilder(client, 5000, message, config.colors.no, "`" + message.author.tag + "`"  + "❌\`Bạn phải tham gia Kênh Voice nào đó\`")
+    if (!client.distube.isPlaying(message)) return functions.embedbuilder(client, 3000, message, config.colors.no, "Không có bài nào đang được phát!")
+    if (!message.member.voice.channel) return functions.embedbuilder(client, 5000, message, config.colors.no, "`" + message.author.tag + "`"  + "Bạn phải tham gia Kênh Voice nào đó\`")
     if (client.distube.isPlaying(message) && message.member.voice.channel.id !== message.member.guild.me.voice.channel.id) return functions.embedbuilder(client, 5000, message, config.colors.no, "`" + message.author.tag + "`"  + "\`Bạn phải tham gia Kênh voice của tôi\`: " + ` \`${message.member.guild.me.voice.channel.name ? message.member.guild.me.voice.channel.name : ""}\``)
     let queue = client.distube.getQueue(message);
-        if(!queue) return functions.embedbuilder(client, 3000, message, config.colors.no, "❌ \`Không có bài nào đang được phát\`")
+        if(!queue) return functions.embedbuilder(client, 3000, message, config.colors.no, "Không có bài nào đang được phát")
 
     if(!args[0]) return functions.embedbuilder(client, 5000, message, config.colors.no, "`" + message.author.tag + "`"  + "\`Vui lòng thêm số thời gian bạn muốn tua\`")
    
@@ -37,4 +37,4 @@ module.exports = {
     return
   }
   };
-  // Vinh đẹp trai bố đời thế 
+  // Wibu Bot
