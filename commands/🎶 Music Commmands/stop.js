0759dac600503b79947e996315278ee94c1b7f7f
khoilcm123 <81813846+khoilcm123@users.noreply.github.com>
2021-06-03 14:25:56 +0700

    Update stop.js

--- commands/🎶 Music Commmands/stop.js
+++ commands/🎶 Music Commmands/stop.js
@@ -1,18 +1,18 @@
 const functions = require("../../functions");
 const config = require("../../config.json")
 module.exports = {
-    name: "stop",
+    name: "leave",
     category: "🎶 Music Commmands",
     aliases: ["leave"],
-    useage: "< prefix > stop",
-  description: "\`Dừng phát nhạc và rời khỏi kênh\`",
+    useage: "< prefix > leave",
+  description: "Dừng phát nhạc và rời khỏi kênh!",
   run: async (client, message, args) => {
               //CHECK IF DJ LOL
               if(client.settings.get(message.guild.id, `djroles`).toString()!==""){
                 let isdj=false;
                 let leftb = "";
                     if(client.settings.get(message.guild.id, `djroles`).join("") === "") 
-                        leftb = "không có Kênh, hay còn gọi là tất cả các Kênh đều là Kênh Bot"
+                        leftb = "Không tìm thấy kênh !"
                     else
                         for(let i = 0; i < client.settings.get(message.guild.id, `djroles`).length; i++){
                                 if(message.member.roles.cache.has(client.settings.get(message.guild.id, `djroles`)[i])) isdj = true;
@@ -22,10 +22,10 @@ module.exports = {
                     if(!isdj && !message.member.hasPermission("ADMINISTRATOR")) return functions.embedbuilder(client, 6000, message, config.colors.no, "DJ-ROLE", `❌ Bạn không có quyền cho Lệnh này! Bạn cần phải có: ${leftb}`)
                 }
                 //CHECK IF DJ LOL
-     if (!message.member.voice.channel) return functions.embedbuilder(client, 5000, message, config.colors.no, "`" + message.author.tag + "`"  + "❌ Bạn phải tham gia Kênh voice nào đó")
+     if (!message.member.voice.channel) return functions.embedbuilder(client, 5000, message, config.colors.no, "`" + message.author.tag + "`"  + "Bạn phải tham gia Kênh voice nào đó! ")
      if (client.distube.isPlaying(message) && message.member.voice.channel.id !== message.member.guild.me.voice.channel.id) return functions.embedbuilder(client, 5000, message, config.colors.no, "`" + message.author.tag + "`"  + "❌ Bạn phải tham gia Kênh voice của tôi: " + ` \`${message.member.guild.me.voice.channel.name ? message.member.guild.me.voice.channel.name : ""}\``)
           
-    functions.embedbuilder(client, "null", message, config.colors.no, "🤧", `OKE tôi đi đây bye bye`)
+    functions.embedbuilder(client, "null", message, config.colors.no, `Bye !`)
     try {
         client.distube.stop(message);
     } catch {
