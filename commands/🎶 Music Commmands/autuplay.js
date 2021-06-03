ac8ad0d6398523142f0031a371ff637ac1e4ecb9
khoilcm123 <81813846+khoilcm123@users.noreply.github.com>
2021-06-03 13:51:51 +0700

    Update autuplay.js

--- commands/🎶 Music Commmands/autuplay.js
+++ commands/🎶 Music Commmands/autuplay.js
@@ -5,29 +5,29 @@ module.exports = {
     category: "🎶 Music Commmands",
     aliases: ["ap","randomsong"],
     useage: "autoplay",
-  description: "😹\`Cho phép tự động phát - các bài hát tương tự ngẫu nhiên\`",
+  description: "Cho phép tự động phát - các bài hát tương tự ngẫu nhiên!",
   run: async (client, message, args) => {
         //CHECK IF DJ LOL
         if(client.settings.get(message.guild.id, `djroles`).toString()!==""){
           let isdj=false;
           let leftb = "";
               if(client.settings.get(message.guild.id, `djroles`).join("") === "") 
-                  leftb = "❌\`không có Kênh, hay còn gọi là tất cả các Kênh đều là Kênh Bot\`"
+                  leftb = "không có Kênh, hay còn gọi là tất cả các Kênh đều là Kênh Bot!"
               else
                   for(let i = 0; i < client.settings.get(message.guild.id, `djroles`).length; i++){
                           if(message.member.roles.cache.has(client.settings.get(message.guild.id, `djroles`)[i])) isdj = true;
                               if(!message.guild.roles.cache.get(client.settings.get(message.guild.id, `djroles`)[i])) continue;
                           leftb += "<@&" + client.settings.get(message.guild.id, `djroles`)[i] + "> | "
                   }
-              if(!isdj && !message.member.hasPermission("ADMINISTRATOR")) return functions.embedbuilder(client, 6000, message, config.colors.no, "DJ-ROLE", `❌ \`Bạn không có quyền để sử dụng Lệnh này! Bạn cần có:\` ${leftb}`)
+              if(!isdj && !message.member.hasPermission("ADMINISTRATOR")) return functions.embedbuilder(client, 6000, message, config.colors.no, "DJ-ROLE", `Bạn không có quyền để sử dụng Lệnh này! Bạn cần có:\` ${leftb}`)
           }
           //CHECK IF DJ LOL
   if (!client.distube.isPlaying(message)) return functions.embedbuilder(client, 3000, message, config.colors.no, "không có gì để phát!")
-    if (!message.member.voice.channel) return functions.embedbuilder(client, 5000, message, config.colors.no, "`" + message.author.tag + "`"  + "❌ \`Bạn phải tham gia Kênh thoại\`")
+    if (!message.member.voice.channel) return functions.embedbuilder(client, 5000, message, config.colors.no, "`" + message.author.tag + "`"  + "Bạn phải tham gia Kênh thoại!")
     if (client.distube.isPlaying(message) && message.member.voice.channel.id !== message.member.guild.me.voice.channel.id) return functions.embedbuilder(client, 5000, message, config.colors.no, "`" + message.author.tag + "`"  + "❌ \`Bạn phải tham gia Kênh Voice của tôi:\` " + ` \`${message.member.guild.me.voice.channel.name ? message.member.guild.me.voice.channel.name : ""}\``)  
     await functions.embedbuilder(client, 3000, message, config.colors.yes, `Autoplay is now on ${client.distube.toggleAutoplay(message) ? "ON" : "OFF"}!`)
     return;
   }
 };
 
-// Vinh đẹp trai bố đời thế 
+// Wibu Bot 
