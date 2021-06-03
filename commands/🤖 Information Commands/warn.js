40fc2ce5a094dd1c507bfd833fb294e9aa3eba75
khoilcm123 <81813846+khoilcm123@users.noreply.github.com>
2021-06-03 19:55:47 +0700

    Update warn.js

--- commands/🤖 Information Commands/warn.js
+++ commands/🤖 Information Commands/warn.js
@@ -8,7 +8,7 @@ module.exports = {
   description: "\`Cảnh báo bất kỳ ai không tuân theo các quy tắc\`",
   run: async (client, message, args) => {
     
-    if(!message.member.hasPermission("MANAGE CHANNELS")) {
+    if(!message.member.hasPermission("ADMINITRASTOR")) {
       return message.channel.send("Bạn nên có quyền quản lí để xài nha!")
     }
     
