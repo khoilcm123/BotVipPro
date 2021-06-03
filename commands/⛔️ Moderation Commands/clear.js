649e82b651c903680f222b8706787331efc89875
khoilcm123 <81813846+khoilcm123@users.noreply.github.com>
2021-06-03 13:40:46 +0700

    Update clear.js

--- commands/⛔️ Moderation Commands/clear.js
+++ commands/⛔️ Moderation Commands/clear.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "clear",
     aliases: ["purge", "nuke", "clean"],
     category: "fun",
-    description: "\`Xoá tin nhắn mhanh theo yêu cầu\`",
+    description: "Xoá tin nhắn theo yêu cầu!",
     usage:"!clear ( số tìn nhắn muốn xoá )",
     
     run: async (client, message, args) => {
@@ -16,28 +16,28 @@ module.exports = {
         }
     
         if (!message.member.hasPermission(perms)) {
-            return message.reply(`❌ \`Ông làm đéo gì có quyền mà đòi xoá tin nhắn của người ta\``)
+            return message.reply("Bạn làm gì có quyền mà đòi xoá tin nhắn của người ta!")
             .then(msg => {
                 msg.delete({ timeout: 15000 })
               })
         }
 
         if (!message.guild.me.hasPermission(perms)) {
-            return message.reply(`❌ \`Tao không có quyền xoá tin nhắn vui lòng cấp quyền cho tao :))\``)
+            return message.reply(`Mình không có quyền xoá tin nhắn vui lòng cấp quyền cho mình!`)
             .then(msg => {
                 msg.delete({ timeout: 15000 })
               })
         }
 
         if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-            return message.reply(`\`Vui lòng cung cấp số từ 1 => 100 để tao còn biết mà xoá chứ pro\``)
+            return message.reply(`Vui lòng cung cấp số từ 1 => 100 để mình còn biết mà xoá nha!`)
             .then(msg => {
                 msg.delete({ timeout: 15000 })
               })
         }
 
         if (parseInt(args[0]) > 100) 
-        return message.reply('\`Hey pro tao chỉ có thể xoá tôi thiểu 100 tin nhắn cùng 1 lúc thôi hơn thì tao chịu :))\`')
+        return message.reply('Mình chỉ có thể xoá tôi thiểu 100 tin nhắn cùng 1 lúc thôi !')
         .then(msg => {
             msg.delete({ timeout: 15000 })
         })
@@ -45,7 +45,7 @@ module.exports = {
         let deleteAmount = parseInt(args[0]);  
 
         message.channel.bulkDelete(deleteAmount, true)
-            .then(deleted => message.channel.send(`❌ \`ĐÃ XOÁ ${deleted.size} TIN NHẮN THEO YÊU CẦU CỦA BẠN\``))
+            .then(deleted => message.channel.send(`Tin nhắn đã bay`))
             .then(msg => {
                 msg.delete({ timeout: 3000 })
               })
