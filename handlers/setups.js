a9a1725a667774914e8ee36c71bb84dcf7b5988c
khoilcm123 <81813846+khoilcm123@users.noreply.github.com>
2021-06-03 11:26:20 +0700

    Add files via upload

---
+++ handlers/setups.js
@@ -0,0 +1,345 @@
+const config = require("../config.json")
+console.log("Loading Setups".brightCyan)
+const functions = require("../functions");
+const guildonlycounter = new Map();
+module.exports = (client) => {
+   
+   let stateswitch = false;
+    client.on("ready", () => {
+        console.log(`${client.user.tag} đã sẵn sàng hoạt động`);
+        setInterval(() => {
+           stateswitch = !stateswitch; //thay đổi trạng thái
+           if (stateswitch) client.user.setActivity(`${config.prefix}help, hhelp để xem chi tiết các lệnh`, { type: "PLAYING" });
+           else client.user.setActivity(`${client.guilds.cache.reduce((c, g) => c + g.memberCount, 0)} Thành viên 😂 | ${client.guilds.cache.size} Server 🤭`, { type: "PLAYING" }); 
+
+        }, 5000); 
+    
+       
+    });
+    client.on("guildCreate", guild => {
+        console.log("Này, tôi được mời vào Server".yellow)
+        client.custom.set(guild.id, {
+            playlists: [],
+        });
+        client.settings.set(guild.id, {
+            prefix: config.prefix,
+            djroles: [],
+            playingembed: "",
+            playingchannel: "",
+            botchannel: [],
+        });
+        client.setups.set(guild.id, {
+            ranking: {
+                enabled: true,
+                backgroundimage: "null",
+            },
+            logger: {
+                enabled: false,
+                channel: "",
+            },
+            welcome: {
+                enabled: false,
+                channel: "",
+                message: `các bạn hãy dành ra chút ít thời gian đọc luật chung của discord để không bị phạt 1 cách ngu người nhé chúc các bạn có khoảng thời gian vui vẻ cùng ae tròng server ❤❤❤`,
+                roles: [],
+                background: {
+                    enable: true,
+                    colors: {
+                        "border": config.colors.yes,
+                        "username-box": config.colors.yes,
+                        "discriminator-box": config.colors.yes,
+                        "message-box": config.colors.yes,
+                        "title": config.colors.yes,
+                        "avatar": config.colors.yes
+                    },
+                    image: "https://images.hdqwalls.com/download/gfx-nerds-uz-2560x1440.jpg"
+                }
+            },
+            leave: {
+                enabled: false,
+                channel: "",
+                message: `Tạm biệt từ: ${guild.name}`,
+                background: {
+                    enable: true,
+                    colors: {
+                        "border": config.colors.no,
+                        "username-box": config.colors.no,
+                        "discriminator-box": config.colors.no,
+                        "message-box": config.colors.no,
+                        "title": config.colors.no,
+                        "avatar": config.colors.no
+                    },
+                    image: "https://cdn.wallpapersafari.com/29/54/7zcTjw.jpg"
+                }
+            },
+            membercount: {
+                enabled: false,
+                channel: "",
+                tempnum: 0,
+                message: "🗣 Tất cả thành viên: {member}"
+            },
+            aichatsystem: {
+                enabled: false,
+                channel: "",
+            },
+            ticketsystem: {
+                enabled: false,
+                guildid: guild.id,
+                messageid: "",
+                channelid: "",
+                parentid: "",
+                message: "Xin chào {user}, cảm ơn bạn đã mở một vé! Sẽ có người giúp bạn sớm!",
+                adminroles: []
+            },
+            jointocreate: {
+                enabled: false,
+                channels: [],
+                tempchannels: [],
+            },
+            counter: {
+                enabled: false,
+                channel: "",
+                number: 0,
+                author: client.user.id
+            }
+        });
+        client.apply.set(guild.id, {
+            "channel_id": "",
+            "f_channel_id": "",
+            "QUESTIONS": [{"1":"DEFAULT"}],
+            "TEMP_ROLE": "",   
+            "accept": "Bạn đã được chấp nhận!",
+            "deny": "Bạn đã bị từ chối!"
+        })
+        client.reactionrole.set(guild.id, 
+            {
+               reactionroles: [
+                   /*
+                   {
+                      MESSAGE_ID: "",
+                      remove_others: false,
+                      Parameters: [ 
+                          { Emoji: "", Role: "",Roles: ["799066077526491186"] }, 
+                      ]
+                      
+                    },
+                    */
+              ]
+            }
+        );
+        getAll(client, guild)
+    })
+    //When a Channel got deleted, try to remove it from the BOTCHANNELS     
+    client.on("channelDelete", function (channel) {
+        try{client.settings.remove(channel.guild.id, channel.id, `botchannel`);}catch{}
+        try{client.setups.remove(channel.guild.id, channel.id, `botchannel`);}catch{}
+        try{if(client.setups.get(channel.guild.id, `channel_id`)===channel.id)client.setups.set(channel.guild.id, "", `channel_id`);}catch{}
+        try{if(client.setups.get(channel.guild.id, `f_channel_id`)===channel.id)client.setups.set(channel.guild.id, "", `f_channel_id`);}catch{}
+        try{if(client.setups.get(channel.guild.id, `logger.channel`)===channel.id)client.setups.set(channel.guild.id, "", `logger.channel`);}catch{}
+        try{if(client.setups.get(channel.guild.id, `welcome.channel`)===channel.id)client.setups.set(channel.guild.id, "", `welcome.channel`);}catch{}
+        try{if(client.setups.get(channel.guild.id, `leave.channel`)===channel.id)client.setups.set(channel.guild.id, "", `leave.channel`);}catch{}
+        try{if(client.setups.get(channel.guild.id, `membercount.channel`)===channel.id)client.setups.set(channel.guild.id, "", `membercount.channel`);}catch{}
+        try{if(client.setups.get(channel.guild.id, `aichatsystem.channel`)===channel.id)client.setups.set(channel.guild.id, "", `aichatsystem.channel`);}catch{}
+        try{if(client.setups.get(channel.guild.id, `ticketsystem.parentid`)===channel.id)client.setups.set(channel.guild.id, "", `ticketsystem.parentid`);}catch{}
+        try{if(client.setups.get(channel.guild.id, `ticketsystem.channelid`)===channel.id)client.setups.set(channel.guild.id, "", `ticketsystem.channelid`);}catch{}
+        try{if(client.setups.get(channel.guild.id, `counter.channel`)===channel.id)client.setups.set(channel.guild.id, "", `counter.channel`);}catch{}
+        try{client.setups.remove(channel.guild.id, channel.id, `jointocreate.channels`);}catch{}
+    });
+    //When a Role got deleted, try to remove it from the DJROLES
+    client.on("roleDelete", function (role) {   
+        try{client.settings.remove(role.guild.id, role.id, `djroles`);}catch{}
+        try{if(client.apply.get(role.guild.id, `TEMP_ROLE`)===role.id)client.setups.set(channel.guild.id, "", `TEMP_ROLE`);}catch{}
+        try{client.settings.remove(role.guild.id, role.id, `welcome.roles`);}catch{}
+        try{client.setups.remove(channel.guild.id, channel.id, `ticketsystem.adminroles`);}catch{}
+    });
+    client.on("message", async message => {
+        if(!message.guild) return;
+        if(message.author.bot) return;
+        client.custom.ensure(message.guild.id, {
+            playlists: [],
+        });
+        client.custom2.ensure(message.author.id, {
+            myplaylists: [],
+        });
+        client.infos.ensure("global", {
+            cmds: 0,
+            songs: 0,
+            filters: 0,
+        })
+        client.settings.ensure(message.guild.id, {
+            prefix: config.prefix,
+            djroles: [],
+            playingembed: "",
+            playingchannel: "",
+            botchannel: [],
+        });
+        client.custom2.ensure(message.author.id, {
+            myplaylists: [],
+        });
+        client.setups.ensure(message.guild.id, {
+            ranking: {
+                enabled: true,
+                backgroundimage: "null",
+            },
+            logger: {
+                enabled: false,
+                channel: "",
+            },
+            welcome: {
+                enabled: false,
+                channel: "",
+                message: `các bạn hãy dành ra chút ít thời gian đọc luật chung của discord để không bị phạt 1 cách ngu người nhé chúc các bạn có khoảng thời gian vui vẻ cùng ae tròng server ❤❤❤`,
+                roles: [],
+                background: {
+                    enable: true,
+                    colors: {
+                        "border": config.colors.yes,
+                        "username-box": config.colors.yes,
+                        "discriminator-box": config.colors.yes,
+                        "message-box": config.colors.yes,
+                        "title": config.colors.yes,
+                        "avatar": config.colors.yes
+                    },
+                    image: "https://images.hdqwalls.com/download/gfx-nerds-uz-2560x1440.jpg"
+                }
+            },
+            leave: {
+                enabled: false,
+                channel: "",
+                message: `Good bye from: ${message.guild.name}`,
+                background: {
+                    enable: true,
+                    colors: {
+                        "border": config.colors.no,
+                        "username-box": config.colors.no,
+                        "discriminator-box": config.colors.no,
+                        "message-box": config.colors.no,
+                        "title": config.colors.no,
+                        "avatar": config.colors.no
+                    },
+                    image: "https://cdn.wallpapersafari.com/29/54/7zcTjw.jpg"
+                }
+            },
+            membercount: {
+                enabled: false,
+                channel: "",
+                tempnum: 0,
+                message: "🗣 Tất cả thành viên: {member}"
+            },
+            aichatsystem: {
+                enabled: false,
+                channel: "",
+            },
+            ticketsystem: {
+                enabled: false,
+                guildid: message.guild.id,
+                messageid: "",
+                channelid: "",
+                parentid: "",
+                message: "Xin chào {user}, cảm ơn bạn đã mở một vé! Sẽ có người giúp bạn sớm!",
+                adminroles: []
+            },
+            jointocreate: {
+                enabled: false,
+                channels: [],
+                tempchannels: [],
+            },
+            counter: {
+                enabled: false,
+                channel: "",
+                number: 0,
+                author: client.user.id
+            }
+        });
+        client.reactionrole.ensure(message.guild.id, 
+            {
+               reactionroles: [
+                   /*
+                   {
+                      MESSAGE_ID: "",
+                      remove_others: false,
+                      Parameters: [ 
+                          { Emoji: "", Role: "",Roles: ["799066077526491186"] }, 
+                      ]
+                      
+                    },
+                    */
+              ]
+            }
+        );
+        client.apply.ensure(message.guild.id, {
+            "channel_id": "",
+            "f_channel_id": "", //changequestions --> which one (lists everyone with index) --> 4. --> Question
+            "QUESTIONS": [{"1":"DEFAULT"}],
+            "TEMP_ROLE": "",   
+            "accept": "You've got accepted!",
+            "deny": "You've got denied!"
+           })
+        
+    });
+    const { MessageEmbed } = require("discord.js");
+    const { stripIndents } = require("common-tags");
+    function getAll(client, guild) {    
+        const embed = new MessageEmbed()
+            .setColor(config.colors.yes)
+            .setTitle('Menu Trợ giúp - CẢM ƠN ĐÃ MỜI TÔI VÀO SERVER!')
+                .addField("**__Người sáng lập:__**", `
+                >>> <@788208207465938954> \`VinhBot#3466\` [facebook](https://www.facebook.com/profile.php?id=100063748987527) | [MỜI BOT](https://discord.com/api/oauth2/authorize?client_id=842012619320983573&permissions=0&scope=bot)
+                `)
+            .setFooter("Để xem mô tả lệnh và kiểu sử dụng < Prefix >help [CMD Name]", client.user.displayAvatarURL())
+            
+            const commands = (category) => {
+                return client.commands
+                    .filter(cmd => cmd.category === category)
+                    .map(cmd => `\`${cmd.name}\``)
+                    .join(", ");
+            }
+        
+            const info = client.categories
+                .map(cat => stripIndents`**__${cat[0].toUpperCase() + cat.slice(1)}__** \n> ${commands(cat)}`)
+                .reduce((string, category) => string + "\n\n" + category);
+        let channel = guild.channels.cache.find(
+            channel =>
+              channel.type === "text" &&
+              channel.permissionsFor(guild.me).has("SEND_MESSAGES")
+          );
+         channel.send(embed.setDescription(info.substr(0,1950) + `\`... để xem tất cả các loại cmds khác: \`${config.prefix}help\``));
+         channel.send("**DISCORD CHÍNH THỨC CỦA BOT:**\nhttps://discord.gg/HGd5uJ7eV8")
+        }
+    client.on('voiceStateUpdate', (oldState,newState) => {
+        if(newState.id === client.user.id && oldState.serverDeaf === true && newState.serverDeaf === false)
+            {
+                try{
+                    const channel = newState.member.guild.channels.cache.find(
+                        channel =>
+                          channel.type === "text" &&
+                          ( channel.name.includes("cmd") ||channel.name.includes("command") ||  channel.name.includes("bot") ) &&
+                          channel.permissionsFor(newState.member.guild.me).has("SEND_MESSAGES")
+                      );
+                      channel.send("\`Đừng bật tiếng tôi! Điều này bảo mật Dữ liệu để mang lại cho bạn trải nghiệm nhanh hơn và mượt mà hơn\`")
+                      let queue = client.distube.getQueue(newState.member.guild.id);
+                      if(!queue) return console.log("NO QUEUE");
+                      queue.connection.voice.setDeaf(true);
+                }catch (error) {
+                    try{
+                        console.log(error)
+                        const channel = newState.member.guild.channels.cache.find(
+                            channel =>
+                              channel.type === "text" &&
+                              channel.permissionsFor(newState.member.guild.me).has("SEND_MESSAGES")
+                          );
+                          channel.send("Đừng bật tiếng tôi!, Điều này bảo mật Dữ liệu để mang lại cho bạn trải nghiệm nhanh hơn và mượt mà hơn")
+                          let queue = client.distube.getQueue(newState.member.guild.id);
+                          if(!queue) return console.log("NO QUEUE");
+                          queue.connection.voice.setDeaf(true);
+                    }catch (error) {
+                        console.log(error)
+                    }
+                }
+                
+        }
+       
+    });
+   
+    console.log("Đã tải thiết lập".brightGreen)
+}
---
+++ handlers/slashcommands.js
@@ -0,0 +1,185 @@
+const config = require("../config.json")
+console.log("LOADING SLASH COMMANDS...".brightCyan)
+const Discord = require("discord.js");
+module.exports = (client) => {
+///////////////////////////////
+/////////SLASH COMMANDS////////
+///////////////////////////////
+client.on('ready', () => {
+   
+//client.api.applications(client.user.id).guilds('guild id').commands.post({data: {     für einen server VVVV ist für mehr als 1 server
+    client.api.applications(client.user.id).commands.post({
+        data: {
+            name: "help",
+            description: "Hiển thị cho bạn thông tin cho mỗi cmd"
+        }
+    });
+    client.api.applications(client.user.id).commands.post({
+        data: {
+            name: "info",
+            description: "Xem một số thông tin về Musicium"
+        }
+    });
+    client.api.applications(client.user.id).commands.post({
+
+        data: {
+            name: "invite",
+            description: "Mời Bot đến máy chủ của riêng bạn và có được trải nghiệm âm nhạc đỉnh cao"
+        }
+    });
+    client.ws.on('INTERACTION_CREATE', async interaction => {
+        let prefix = await client.settings.get(interaction.guild_id, `prefix`);
+        if (prefix === null) prefix = config.prefix;
+
+        const command = interaction.data.name.toLowerCase();
+        const args = interaction.data.options;
+	let inviteembed = new Discord.MessageEmbed()
+	.setColor(config.colors.yes)
+	.setTitle("Invite me now!")
+	.setDescription(`[Mời Bot vào server](https://discord.com/api/oauth2/authorize?client_id=842012619320983573&permissions=0&scope=bot%20applications.commands)
+        
+        [DISCORD SERVER](https://discord.gg/HGd5uJ7eV8)
+           |   [FACEBOOK](https://www.facebook.com/profile.php?id=100063748987527)   |   🇻🇳 Cảm ơn vì đã mời!`)
+	.setFooter(client.user.username + " | Syntax:  <>...must    []...optional", client.user.displayAvatarURL())
+	.setAuthor(interaction.member.user.username,  client.user.displayAvatarURL(), "hế hế")
+	let totalMembers = client.guilds.cache.reduce((c, g) => c + g.memberCount, 0);
+            let days = Math.floor(client.uptime / 86400000);
+            let hours = Math.floor(client.uptime / 3600000) % 24;
+            let minutes = Math.floor(client.uptime / 60000) % 60;
+            let seconds = Math.floor(client.uptime / 1000) % 60;
+
+            const infoembed = new Discord.MessageEmbed()
+                .setAuthor(
+                    `Thông tin về: ${client.user.username} Bot`,
+                    client.user.displayAvatarURL(), "https://discord.com/api/oauth2/authorize?client_id=842012619320983573&permissions=0&scope=bot%20applications.commands"
+                )
+                .setColor(config.colors.yes)
+                .addFields(
+                    {
+                        name: 'NOT TAG',
+                        value: `\`${client.user.tag}\``,
+                        inline: true,
+                    },
+                    {
+                        name: 'Version',
+                        value: `\`7.0.0\``,
+                        inline: true,
+                    },
+                    {
+                        name: "prefix lệnh",
+                        value: `\`${prefix}\``,
+                        inline: true,
+                    },
+          
+                    {
+                        name: 'Thời gian kể từ lần khởi động lại gần đây nhất',
+                        value: `\`${process.uptime().toFixed(2)}s\``,
+                        inline: true,
+                    },
+                    {
+                        name: 'Uptime',
+                        value: `\`${days}Ngày\` \`${hours}Giờ\` \`${minutes}Phút\` \`${seconds}Giây\``,
+                        inline: true,
+                    },
+                    {
+                        name: 'Số lượng server',
+                        value: `\`${client.guilds.cache.size}\``,
+                        inline: true,
+                    },
+                    {
+                        name: 'Tổng số thành viên',
+                        value: `**\`${totalMembers}\`**`,
+                        inline: true,
+                    },
+                    {
+                        name: 'Chủ sở hữu và Nhà phát triển',
+                        value: `\`VinhBot#3466\` <@788208207465938954>`,
+                        inline: true,
+                    },
+                )
+                .addField("\u200b", `
+            \u200b
+            `)
+                .addField("***Thông tin OWNER:***", `
+            >>> <@788208207465938954> \`VinhBot#3466\` [FACEBOOK](https://www.facebook.com/profile.php?id=100063748987527)
+            `)
+                .addField("***ỦNG HỘ:***", `
+            >>> [DISCORD](https://discord.gg/HGd5uJ7eV8) | [MỜI BOT](https://discord.com/api/oauth2/authorize?client_id=842012619320983573&permissions=0&scope=bot%20applications.commands/)
+            `)
+            const helpembed = new Discord.MessageEmbed()
+            .setColor(config.colors.yes)
+            .setTitle(`Help Menu\nPrefix: \`${prefix}\``)
+                .addField("\u200b","\u200b")
+                .addField("**THÔNG TIN BỘ LỌC BASSBOOST**", `
+                >>> Bây giờ bạn có thể thay đổi mức tăng Bassboost của bạn từ \`1\`-\`20\`! Thí dụ: \`${prefix}bassboost 10\` *Tạo ra một Bassboost với 10db Tăng*
+                `)
+                .addField("**DANH SÁCH PHÁT TÙY CHỈNH TRƯỚC**", `
+                >>> có một số danh sách phát tùy chỉnh mà bạn có thể phát, mỗi danh sách có 75 bài hát!
+                
+                1. Charts
+                2. Christmas
+                3. Jazz
+                4. Blues
+                5. Country
+                6. Rock
+                *nhiều hơn sắp ra mắt*
+                \`Sử dụng lệnh: ${prefix}playlist <Playlist Number.>\`
+                `)
+                .addField("**RADIO STATIONS**", `
+                >>> có hơn 200 đài phát thanh có sẵn, bạn có thể xem chúng bằng cách nhập: \`${prefix}radio\`
+                and play them by \`${prefix}radio <stationnum.>\`
+                `)
+                .addField("\u200b","\u200b")
+                .addField("**__Thông tin OWMER:__**", `
+                >>> <@788208207465938954> \`Vinhbot#3466\` [FACEBOOK](https://www.facebook.com/profile.php?id=100063748987527) | [MỜI BOT](https://discord.com/api/oauth2/authorize?client_id=842012619320983573&permissions=0&scope=bot%20applications.commands)
+                `)
+             
+            .setFooter(`Để xem mô tả lệnh và kiểu sử dụng ${prefix}help [CMD Name]`, client.user.displayAvatarURL())
+            
+        const commands = (category) => {
+            return client.commands
+                .filter(cmd => cmd.category === category)
+                .map(cmd => `\`${cmd.name}\``)
+                .join(", ");
+        }
+    
+        const info = client.categories
+            .map(cat => stripIndents`**__${cat[0].toUpperCase() + cat.slice(1)}__** \n> ${commands(cat)}`)
+            .reduce((string, category) => string + "\n\n" + category);
+            helpembed.setDescription(info.substr(0, 1900)+ `... để xem tất cả lệnh, hãy nhập: \`${prefix}help\``);
+        if(command == 'help') {
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: await createAPIMessage(interaction, helpembed)
+                }
+            });
+        }
+		 if(command == 'invite') {
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: await createAPIMessage(interaction, inviteembed)
+                }
+            });
+        }
+		if(command == 'info') {
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: await createAPIMessage(interaction, infoembed)
+                }
+            });
+        }
+    });
+});
+
+async function createAPIMessage(interaction, content) {
+    const apiMessage = await Discord.APIMessage.create(client.channels.resolve(interaction.channel_id), content)
+        .resolveData()
+        .resolveFiles();
+    
+    return { ...apiMessage.data, files: apiMessage.files };
+}
+ console.log('Lệnh Slash đã tải'.brightGreen);
+}
