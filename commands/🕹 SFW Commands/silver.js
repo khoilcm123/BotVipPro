3b329253cca2a5b66c818b3e750cfcc852f9ed6a
Khôi Lâm <81813846+khoilcm123@users.noreply.github.com>
2021-11-06 11:43:24 +0700

    Create silver.js

---
+++ commands/🕹 SFW Commands/silver.js
@@ -0,0 +1,22 @@
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+const { readdirSync, readFileSync } = require('fs');
+
+module.exports = {
+    name: 'silver',
+    category: 'vui',
+    description: '\`Ảnh Silver ( Thêm vào cho có )\`',
+    aliases: ['bac', 'silver'],
+    usage:"!silver",
+    cooldown: 3,
+    run: async (client, message, args) => {
+        const folder = readdirSync("././assets/silver");
+        const randomFile = folder[Math.floor(Math.random() * folder.length)];
+        const file = readFileSync(`././assets/silver/${randomFile}`);
+        const ext = randomFile.slice(-3);
+        const attachment = new MessageAttachment(file, `silver.${ext}`);
+        const embed = new MessageEmbed()
+            .attachFiles(attachment)
+            .setImage(`attachment://silver.${ext}`);
+        message.channel.send(embed);
+    },
+};
